8b1b94ae562f20e4cbcedc7bd763d03c082dee08
Mark Dillon <markdillon307@gmail.com>
2023-01-23 21:22:10 +0800

    Update

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ TEMPLATE_DIR = Path(BASE_DIR) / "templates"
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=n19=f495zhto@w-0g5hr*yjv3-d+kvqdsncqva!i2is%2-ah#"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
